73b13b2e8cff1c9e8a99bdaa85011f6109a7abd7
Fausto <f.ferla@caninosrl.it>
2021-09-22 10:45:10 +0200

    prova react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+
+    super(props);
+    this.state={testoprova:"prova"}
+  }
+render(){return (
+  <div className="search">
+ 
+  <input type="button" value="prova" onClick={() => this.handleClick()} />
+  {this.state.testoprova}
+</div>)
+}
+handleClick() {
+  this.setState({testoprova:"prova modificate"});
+  console.log('Il valore di `this` è: ', this);
+}
+
+
 }
 
 export default App;
