13a1230f2017a8994bf7fbabc4df9d2477b54d76
Fausto <f.ferla@caninosrl.it>
2021-09-22 17:48:56 +0200

    action button search api

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,33 @@ class App extends React.Component{
   constructor(props){
 
     super(props);
-    this.state={testoprova:"prova"}
+    this.state={testoprova:"prova"};
+    this.handleClick = this.handleClick.bind(this);
+    this.letturaDati = this.letturaDati.bind(this);
+
   }
 render(){return (
   <div className="search">
  
-  <input type="button" value="prova" onClick={() => this.handleClick()} />
+  <input type="button" value="prova" onClick={this.handleClick} />
   {this.state.testoprova}
 </div>)
 }
+
 handleClick() {
-  this.setState({testoprova:"prova modificate"});
+  this.setState({testoprova:this.state.testoprova+" prova modificate"}, () => { this.letturaDati()} );
   console.log('Il valore di `this` è: ', this);
+  
+}
+
+letturaDati(){
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => {
+    this.setState({testoprova:this.state.testoprova + json.title});
+  });
+  
+
 }
 
 
