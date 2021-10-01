8a53f8e511715e420beb7cceb4271385c3a5e618
Fausto <f.ferla@caninosrl.it>
2021-10-01 12:45:01 +0200

    build dentro lumen

--- client/src/controlli.js
+++ client/src/controlli.js
@@ -4,7 +4,7 @@ export const letturaDati = ChangeEvent => {
 console.log(ChangeEvent.target.value);
 
 if(ChangeEvent.target.value.length > 2){
-return (fetch('http://localhost:8000/api?filtro='+ChangeEvent.target.value)
+return (fetch('/api?filtro='+ChangeEvent.target.value)
     .then(response => response.json())
     .then(json =>  {
       return json;
